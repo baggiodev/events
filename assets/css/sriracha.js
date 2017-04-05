b738f59941767477b7e0cc03dc02a7ded20b591c
Baggio Shehadi <k3nh1n@yahoo.com>
2017-04-05 14:11:28 -0500

    added ignore and deleted random js

--- assets/css/sriracha.js
+++
@@ -1,9 +0,0 @@
-console.log(parseFloat(process.argv[2]) = parseFloat(process.argv[3]));
-var a = process.argv[2];
-var b = process.argv[3];
-
-if (a === b) {
-	console.log(true);
-} else {
-	console.log(false);
-};
